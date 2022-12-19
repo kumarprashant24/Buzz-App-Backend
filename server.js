2f2f3f45783472b2a2b6d0b4b2b79267c78b848e
kumarprashant24 <kumar.prashant@tothenew.com>
2022-12-19 16:05:08 +0530

    removed home routes

--- server.js
+++ server.js
@@ -131,11 +131,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Routes
-app.get('/', (req, res) => {
-
-  res.send("hello world");
- 
-});
 app.use('/api', require('./routes/index'));
 
 mongoose.connect(MONGO_URI, (err) => {
